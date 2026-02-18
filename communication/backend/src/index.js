d2a8d596e18df384c89a9ba9c8ae9317ace505de
nooblancer <kushagraa0103@gmail.com>
2026-02-18 19:17:36 +0530

    backend

--- communication/backend/src/index.js
+++ communication/backend/src/index.js
@@ -1,17 +1,33 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
-import authRoutes from './routes/auth.route.js';
+
+import path from 'path';
+
 import { connectDB } from './lib/db.js';
+
+import authRoutes from './routes/auth.route.js';
+import messageRoutes from './routes/message.route.js';
+import { app, server } from './lib/socket.js';
 
 dotenv.config();
 
-const app = express();
 const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
 
-app.use("/api/auth", authRoutes)
 app.use(express.json());
 app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+};
 
 app.listen(PORT, () => {
     console.log('server is running on PORT: ' + PORT);
